25fa801cfddba88afecec8972ed13a7ed7725449
Javid Mohajery <Javid.Mohajery@external.iherb.com>
2024-02-19 00:30:40 +0300

    send parameter to component - deconstruct params

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import ListGroup from "./Components/ListGroup";
 
+function itemSelect(item: string){
+  console.log(item + " is selected")
+}
 function App() {
   const items = ["Item1", "Items2", "Items3"] 
-  return <ListGroup items={items} heading="Cities"/>
+  return <ListGroup items={items} heading="Cities" itemSelected={itemSelect}/>
 }
 
 export default App
--- src/Components/ListGroup.tsx
+++ src/Components/ListGroup.tsx
@@ -1,14 +1,29 @@
+import { useState } from "react"
+
 interface Props{
   items: string[],
-  heading: string
+  heading: string,
+  itemSelected : (item: string) => void
 }
-function ListGroup({items, heading}: Props){
-    
+function ListGroup({items, heading, itemSelected}: Props){
+    let [selectedIndex, setIndex] = useState(0)
+    function getClassNames(index: Number) {
+      if(index === selectedIndex) return "list-group-item active" 
+      else return "list-group-item" 
+    } 
     return(
         <>
         <h1>{heading}</h1>
         <ul className="list-group">
-          {items.map(i => <li key={i} className="list-group-item" onClick={(event) => console.log(event)}>{i}</li>)}
+          {items.map((item, index) => 
+          <li key={index} className={getClassNames(index)}
+                onClick={() => {
+                                setIndex(index); 
+                                itemSelected(item)
+                              }
+                        }>
+                    {item}
+          </li>)}
   </ul>
         </>
     ) 
